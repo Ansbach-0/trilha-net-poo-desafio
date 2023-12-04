d09f3d4486eebf0ea223534351c225abd7a7b42a
Ansbach-0 <ans.vinycius@gmail.com>
2023-12-04 15:48:53 -0300

    initial release

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testes Iphone:");
+Smartphone iphone = new Iphone("XXXXXXXXXXX", "Model S", "1223334444", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("VSCODE MOBILE");
+//
+Console.WriteLine("Testes Nokia:");
+Smartphone nokia = new Nokia("XXXXXXXXXXX", "Nokia Tijolao", "4444333221", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("NEW CODE");
